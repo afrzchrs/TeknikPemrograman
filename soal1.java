cdfa586206fa69ed376812f4bfa30d3d046bb146
Afriza Choirie Saputra <afrizacho@gmail.com>
2025-02-13 11:49:18 +0700

    first commit

---
+++ soal1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author ASPIRE 5
+ */
+public class soal1 {
+    public static void main(String args[]) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1 + angka2);
+
+        System.out.println("Hasil 1 = " + hasil);
+    }
+}
